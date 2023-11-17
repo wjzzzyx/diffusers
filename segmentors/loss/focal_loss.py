6f14fca84ac3037a09daca6ef564f2496b0b42d8
Yixiao Zhang <zyx@Yixiaos-MacBook-Air.local>
2023-11-16 21:01:45 -0800

    Fix bugs in focal loss.

--- segmentors/loss/focal_loss.py
+++ segmentors/loss/focal_loss.py
@@ -7,32 +7,29 @@ from typing import Optional
 def focal_loss(
     logits: torch.Tensor,
     targets: torch.Tensor,
-    alpha: float,
     gamma: float,
     weight: Optional[torch.Tensor] = None,
     reduction: str = 'mean',
 ) -> torch.Tensor:
     """
     Args:
-        logits: (B, C, H, W)
-        targets: (B, H, W)
-        alpha: (C)
-        gamma:
-        weight: (C)
-        reduction:
+        logits: (B, C, H, W) unnormalized logits output from the model
+        targets: (B, H, W) class indexes in the range [0, C)
+        gamma: exponent
+        weight: (C) one-dim tensor, weight for each class
+        reduction: choices in ('mean', 'sum', 'none')
     Returns:
         loss
     """
     eps = 1e-4
-    prob = F.softmax(logits.permute(0, 2, 3, 1), dim=-1)
-    prob_gt = prob[targets]    # (B, H, W)
-    logprob_gt = torch.log(prob + eps)
-    # alpha_gt = alpha[targets]    # (B, H, W)
+    prob = F.softmax(logits, dim=1)
+    prob_gt = torch.gather(prob, 1, targets.unsqueeze(1)).squeeze(1)    # (B, H, W)
+    logprob_gt = torch.log(prob_gt + eps)
     if weight is None:
         weight = torch.ones(targets.shape, dtype=logits.dtype, device=logits.device)
     else:
         weight = weight[targets]
-    loss = -alpha * torch.pow(1 - prob_gt, gamma) * logprob_gt
+    loss = - weight * torch.pow(1 - prob_gt, gamma) * logprob_gt
 
     if reduction == 'mean':
         return loss.mean()
@@ -54,7 +51,10 @@ class FocalLoss(nn.Module):
         self.gamma = gamma
     
     def forward(self, logits, targets):
-        return focal_loss(logits, targets, weight=self.weight, reduction=self.reduction)
+        return focal_loss(
+            logits, targets, alpha=self.alpha, gamma=self.gamma,
+            weight=self.weight, reduction=self.reduction
+        )
 
 
 if __name__ == '__main__':
