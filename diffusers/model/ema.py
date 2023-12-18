533be92c169e8b1803d3b87398dc551daeddf9c7
Yixiao Zhang <zyx@Yixiaos-MacBook-Air.local>
2023-12-17 17:51:28 -0800

    EMA helper.

--- diffusers/model/ema.py
+++ diffusers/model/ema.py
@@ -75,3 +75,38 @@ class LitEma(nn.Module):
         """
         for c_param, param in zip(self.collected_params, parameters):
             param.data.copy_(c_param.data)
+
+
+class EMAHelper():
+    def __init__(self, max_decay=0.9999, min_decay=0.0, update_after_step=0, use_ema_warmup=False, inv_gamma=1.0, power=2/3):
+        self.max_decay = max_decay
+        self.min_decay = min_decay
+        self.update_after_step = update_after_step
+        self.use_ema_warmup = use_ema_warmup
+        self.inv_gamma = inv_gamma
+        self.power = power
+        self.num_updates = 0
+        self.decay = 0.0
+    
+    @torch.no_grad()
+    def ema_step(self, model, model_ema):
+        # find the current step decay value
+        self.num_updates += 1
+        step = self.num_updates - self.update_after_step - 1
+        if step <= 0:
+            decay = 0.0
+        else:
+            if self.use_ema_warmup:
+                decay = 1 - (1 + step / self.inv_gamma) ** (-self.power)
+            else:
+                decay = (1 + step) / (10 + step)
+        decay = min(decay, self.max_decay)
+        decay = max(decay, self.min_decay)
+        self.decay = decay
+
+        for (name, param), (name_ema, param_ema) in zip(model.named_parameters(), model_ema.named_parameters()):
+            assert(name == name_ema)
+            if param.requires_grad:
+                param_ema.sub_((1 - decay) * (param_ema - param))
+            else:
+                param_ema.copy_(param)
