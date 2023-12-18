9d650b8fd45cf4f2ba99185bb28dd9c50672eb17
Yixiao Zhang <zyx@Yixiaos-MacBook-Air.local>
2023-12-17 17:51:04 -0800

    DDIM unconditional image generation pipeline.

--- diffusers/pipeline/uncond_image_gen.py
+++ diffusers/pipeline/uncond_image_gen.py
@@ -19,3 +19,22 @@ def pipeline_ddpm(
         image = sampler.step(output, t, image, generator=generator)
     
     return image
+
+
+def pipeline_ddim(
+    model,
+    sampler,
+    batch_size: int,
+    image_shape: Sequence,
+    num_inference_steps: int,
+    generator = None
+):
+    image = torch.randn((batch_size, model.in_channels, *image_shape), generator=generator)
+
+    sampler.set_timesteps(num_inference_steps)
+
+    for t in range(sampler.timesteps):
+        output = model(image, t)
+        image = sampler.step(output, t, image, generator=generator)
+    
+    return image
