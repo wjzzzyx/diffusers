7c69bcd8ca436424ea254d519e515e7c29fad3bd
Yixiao Zhang <zyx@Yixiaos-MacBook-Air.local>
2023-12-30 21:24:15 -0800

    Update DDIM and DDIMInverse samplers.

--- diffusers/sampler/sample_ddim.py
+++ diffusers/sampler/sample_ddim.py
@@ -25,6 +25,9 @@ class DDIMSampler():
     
     def set_timesteps(self, num_inference_steps):
         self.num_inference_steps = num_inference_steps
+        # leading spacing
+        self.timesteps = range(0, self.num_train_steps, self.num_train_steps // self.num_inference_steps)
+        self.timesteps = self.timesteps[::-1]
     
     def step(self, output: Dict[str, torch.Tensor], t: int, generator=None):
         t_next = t - self.num_train_steps // self.num_inference_steps
@@ -46,10 +49,25 @@ class DDIMSampler():
         
         return sample_next
     
-    def sample(self, model, batch_size: int, image_shape: Sequence, generator=None):
+    def sample(
+        self,
+        model,
+        batch_size: int,
+        image_shape: Sequence,
+        cond_pos_prompt: torch.Tensor = None,
+        cond_neg_prompt: torch.Tensor = None,
+        generator=None
+    ) -> torch.Tensor:
         image = torch.randn((batch_size, *image_shape), generator=generator, device=model.device)
+
+        extra_args = dict()
+        if cond_pos_prompt is not None:
+            extra_args['cond_pos_prompt'] = cond_pos_prompt
+        if cond_neg_prompt is not None:
+            extra_args['cond_neg_prompt'] = cond_neg_prompt
+        
         for t in self.timesteps:
-            output = model(image, t)
+            output = model(image, t, **extra_args)
             if model.prediction_type == 'epsilon':
                 epsilon = output
                 sample = (image - torch.sqrt(1 - self.alphas_cumprod[t]) * output) / torch.sqrt(self.alphas_cumprod[t])
@@ -77,10 +95,10 @@ class DDIMInverseSampler():
     
     def set_timesteps(self, num_inference_steps):
         self.num_inference_steps = num_inference_steps
+        # leading spacing
+        self.timesteps = range(0, self.num_train_steps, self.num_train_steps // self.num_inference_steps)
     
     def step(self, output, t_next: int):
-        t = t - self.num_train_steps // self.num_inference_steps
-
         alpha_cumprod_t_next = self.alphas_cumprod[t_next]
 
         pred_x0 = output['sample']
@@ -90,3 +108,23 @@ class DDIMInverseSampler():
         sample_next = torch.sqrt(alpha_cumprod_t_next) * pred_x0 + torch.sqrt(1 - alpha_cumprod_t_next) * pred_z
 
         return sample_next
+    
+    def sample(
+        self,
+        model,
+        image: torch.Tensor,
+    ) -> torch.Tensor:
+        for it in self.timesteps:
+            t = self.timesteps[it]
+            t_next = self.timesteps[it+1] if it < len(self.timesteps) else self.num_train_steps - 1
+            output = model(image, t)
+            if model.prediction_type == 'epsilon':
+                epsilon = output
+                sample = (image - torch.sqrt(1 - self.alphas_cumprod[t]) * output) / torch.sqrt(self.alphas_cumprod[t])
+            elif model.prediction_type == 'sample':
+                sample = output
+                epsilon = (image - torch.sqrt(self.alphas_cumprod[t]) * output) / torch.sqrt(1 - self.alphas_cumprod[t])
+            elif model.prediction_type == 'v_prediction':
+                raise NotImplementedError('v_prediction is not implemented for DDPM.')
+            image = self.step({'epsilon': epsilon, 'sample': sample}, t_next)
+        return image
