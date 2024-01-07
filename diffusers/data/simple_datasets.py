b4122a9a2e0d5242d959f2753a899fa8d5d9fece
Yixiao Zhang <zyx@Yixiaos-MacBook-Air.local>
2024-01-07 11:21:12 -0800

    Add ImageFolder dataset class.

--- diffusers/data/simple_datasets.py
+++ diffusers/data/simple_datasets.py
@@ -1,3 +1,7 @@
+import os
+from PIL import Image
+import torchvision.transforms.v2 as T
+import torchvision.transforms.v2.functional as TF
 from torch.utils.data import Dataset
 
 
@@ -10,3 +14,19 @@ class NullImageDataset(Dataset):
     
     def __getitem__(self, index):
         return {'index': index}
+
+
+class ImageFolder(Dataset):
+    def __init__(self, image_dir):
+        self.image_dir = image_dir
+        self.fnames = sorted(os.listdir(image_dir))
+    
+    def __len__(self):
+        return len(self.fnames)
+    
+    def __getitem__(self, index):
+        fpath = os.path.join(self.image_dir, self.fnames[index])
+        image = Image.open(fpath)
+        image = TF.pil_to_tensor(image)
+        image = image.float() / 255
+        return {'image': image, 'fname': self.fnames[index]}
