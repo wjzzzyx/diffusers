0ac1430bc8d104f85f5c9d38be7260e6998e9117
Yixiao Zhang <zyx@Yixiaos-MacBook-Air.local>
2024-03-25 21:29:36 -0700

    Add CLIPEncoderWeighted for stable diffusion, to process prompt with weights. Also fix bugs.

--- diffusers/model/text_encoder.py
+++ diffusers/model/text_encoder.py
@@ -1,3 +1,4 @@
+import re
 import torch
 import torch.nn as nn
 from transformers import CLIPTokenizer, CLIPTextModel
@@ -55,32 +56,12 @@ class CLIPEncoderUnlimited(nn.Module):
         max_num_chunks = max([len(chunks) for chunks in batch_chunks])
         empty_chunk = [self.tokenizer.bos_token_id] + [self.tokenizer.eos_token_id] * (self.tokenizer.model_max_length - 1)
         for chunks in batch_chunks:
-            if len(chunks) < max_num_chunks:
+            while len(chunks) < max_num_chunks:
                 chunks.append(empty_chunk)
         return batch_chunks
 
     def _split_chunks(self, tokens):
-        split_points = list()
-        pos = 0
-        last_comma = -1
-        i = 0
-        while i < len(tokens):
-            if tokens[i] == self.stop_token_id:
-                split_points.append(i)
-                pos = 0
-                i += 1
-            elif tokens[i] == self.comma_token_id:
-                last_comma = i
-                pos += 1
-                i += 1
-            elif pos == self.tokenizer.model_max_length - 2:
-                split_points.append(last_comma)
-                pos = 0
-                i = last_comma + 1
-            else:
-                pos += 1
-                i += 1
-        
+        split_points = self.locate_split_points(tokens)
         chunks = list()
         for j in range(len(split_points)):
             p = split_points[j]
@@ -101,6 +82,29 @@ class CLIPEncoderUnlimited(nn.Module):
             chunks[ichunk] = [self.tokenizer.bos_token_id] + chunks[ichunk] + [self.tokenizer.eos_token_id]
         
         return chunks
+    
+    def _locate_split_points(self, tokens):
+        split_points = list()
+        pos = 0
+        last_comma = -1
+        i = 0
+        while i < len(tokens):
+            if tokens[i] == self.stop_token_id:
+                split_points.append(i)
+                pos = 0
+                i += 1
+            elif tokens[i] == self.comma_token_id:
+                last_comma = i
+                pos += 1
+                i += 1
+            elif pos == self.tokenizer.model_max_length - 2:
+                split_points.append(last_comma)
+                pos = 0
+                i = last_comma + 1
+            else:
+                pos += 1
+                i += 1
+        return split_points
 
     def encode_with_transformers(self, batch_tokens):
         batch_tokens = torch.asarray(batch_tokens, device=self.device)
@@ -111,3 +115,105 @@ class CLIPEncoderUnlimited(nn.Module):
         else:
             h = outputs.last_hidden_state
         return h
+
+
+class CLIPEncoderWeighted(CLIPEncoderUnlimited):
+    def forward(self, texts):
+        texts_with_weights = self.parse_weighted_text(texts)
+        tokens, weights = self.get_tokens(texts_with_weights)
+        chunks, chunk_weights = self.split_chunks(tokens, weights)    # list(batch) of list(chunk)
+        chunks_input = list(map(list, zip(*chunks)))    # list(chunk) of list(batch)
+
+        text_embs = list()
+        for chunk in chunks_input:
+            emb = self.encode_with_transformers(chunk)
+            text_embs.append(emb)
+        text_embs = torch.concatenate(text_embs, dim=1)    # shape (batch, seq, emb)
+        
+        weights = torch.asarray(chunk_weights, device=self.device).view(len(chunk_weights), -1)
+        original_mean = text_embs.mean(dim=(1, 2))
+        text_embs = text_embs * weights.unsqueeze(-1)
+        new_mean = text_embs.mean(dim=(1, 2))
+        text_embs = text_embs * (original_mean / new_mean)[:, None, None]
+
+        return text_embs
+
+    def parse_weighted_text(self, texts):
+        pattern = re.compile(r'\((.*?)\:(\d+(?:\.\d+)?)\)')
+        texts_with_weights = list()
+
+        for text in texts:
+            text_with_weights = list()
+            last = 0
+            matches = pattern.finditer(text)
+            for m in matches:
+                m_text, m_weight = m.group(0).strip('()').split(':')
+                m_text = m_text.strip()
+                m_weight = float(m_weight.strip())
+                text_with_weights.append((text[last:m.start()], 1.0))
+                text_with_weights.append((text[m_text], m_weight))
+                last = m.end() + 1
+            text_with_weights.append((text[last:], 1.0))
+
+            texts_with_weights.append(text_with_weights)
+        
+        return texts_with_weights
+
+    def get_tokens(self, texts_with_weights):
+        batch_tokens = list()
+        batch_weights = list()
+        for text_with_weights in texts_with_weights:
+            tokens = list()
+            weights = list()
+            for phrase, weight in text_with_weights:
+                phrase_tokens = self.tokenizer(phrase, truncation=False, add_special_tokens=False).input_ids
+                tokens.extend(phrase_tokens)
+                weights.extend([weight] * len(phrase_tokens))
+            batch_tokens.append(tokens)
+            batch_weights.append(weights)
+        return batch_tokens, batch_weights
+    
+    def split_chunks(self, batch_tokens, batch_weights):
+        batch_chunks = list()
+        batch_chunk_weights = list()
+
+        for tokens, weights in zip(batch_tokens, batch_weights):
+            chunks, chunk_weights = self._split_chunks(tokens, weights)
+            batch_chunks.append(chunks)
+            batch_chunk_weights.append(chunk_weights)
+        
+        max_num_chunks = max([len(chunks) for chunks in batch_chunks])
+        empty_chunk = [self.tokenizer.bos_token_id] + [self.tokenizer.eos_token_id] * (self.tokenizer.model_max_length - 1)
+        empty_chunk_weight = [0.0] * self.tokenizer.model_max_length
+        for chunks, chunk_weights in zip(batch_chunks, batch_chunk_weights):
+            while len(chunks) < max_num_chunks:
+                chunks.append(empty_chunk)
+                chunk_weights.append(empty_chunk_weight)
+        
+        return batch_chunks, batch_chunk_weights
+
+    def _split_chunks(self, tokens, weights):
+        split_points = self.locate_split_points(tokens)
+        chunks = list()
+        chunk_weights = list()
+        last = 0
+
+        for p in split_points:
+            chunks.append(tokens[last:p+1])
+            chunk_weights.append(weights[last:p+1])
+            last = p + 1
+        chunks.append(tokens[last:])
+        chunk_weights.append(weights[last:])
+
+        # pad each chunk to be model_max_length
+        for ichunk in range(len(chunks)):
+            while len(chunks[ichunk]) < self.tokenizer.model_max_length - 2:
+                chunks[ichunk].append(self.tokenizer.eos_token_id)
+                chunk_weights[ichunk].append(0.0)
+        
+        # add bos and eos tokens to each chunk
+        for ichunk in range(len(chunks)):
+            chunks[ichunk] = [self.tokenizer.bos_token_id] + chunks[ichunk] + [self.tokenizer.eos_token_id]
+            chunk_weights[ichunk] = [0.0] + chunk_weights[ichunk] + [0.0]
+        
+        return chunks, chunk_weights
